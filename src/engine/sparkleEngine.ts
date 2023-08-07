e84fbfc816b966d5831e401603e0ca95bcc372a1
hardikphalet <hardik.phalet@gmail.com>
2023-08-08 03:52:12 +0530

    Added like operation

--- src/engine/sparkleEngine.ts
+++ src/engine/sparkleEngine.ts
@@ -1,5 +1,5 @@
-import { Operation } from './consts.js';
-import { SparkleObject } from './interfaces.js';
+import {Operation} from './consts.js';
+import {SparkleObject} from './interfaces.js';
 
 export function convert(objects: SparkleObject[]): string {
   let resultString: string = '';
@@ -13,45 +13,47 @@ function equalsOperation(object: SparkleObject): string {
   if (object.values.length !== 1) {
     throw Error('Equal operation strictly requires 1 value');
   }
-  const resultString: string = object.bind.concat('.eq:').concat(object.values[0]);
-  return resultString;
+  return object.bind.concat('.eq:').concat(object.values[0]);
+}
+
+function likeOperation(object: SparkleObject): string {
+  if (object.values.length !== 1) {
+    throw Error('Equal operation strictly requires 1 value');
+  }
+  return object.bind.concat('.like:').concat(object.values[0]);
 }
 
 function greaterThanOperation(object: SparkleObject): string {
   if (object.values.length !== 1) {
     throw Error('Operation strictly requires 1 value');
   }
-  const resultString: string = object.bind.concat('.gt:').concat(object.values[0]);
-  return resultString;
+  return object.bind.concat('.gt:').concat(object.values[0]);
 }
 
 function lesserThanOperation(object: SparkleObject): string {
   if (object.values.length !== 1) {
     throw Error('Operation strictly requires 1 value');
   }
-  const resultString: string = object.bind.concat('.lt:').concat(object.values[0]);
-  return resultString;
+  return object.bind.concat('.lt:').concat(object.values[0]);
 }
 
 function lesserThanOrEqualsOperation(object: SparkleObject): string {
   if (object.values.length !== 1) {
     throw Error('Operation strictly requires 1 value');
   }
-  const resultString: string = object.bind.concat('.le:').concat(object.values[0]);
-  return resultString;
+  return object.bind.concat('.le:').concat(object.values[0]);
 }
 
 function greaterThanOrEqualsOperation(object: SparkleObject): string {
   if (object.values.length !== 1) {
     throw Error('Operation strictly requires 1 value');
   }
-  const resultString: string = object.bind.concat('.ge:').concat(object.values[0]);
-  return resultString;
+  return object.bind.concat('.ge:').concat(object.values[0]);
 }
 
 function inOperation(object: SparkleObject): string {
   if (object.values.length < 1) {
-    throw Error('Operation requires atleast one value');
+    throw Error('Operation requires at least one value');
   }
   let resultString: string = object.bind.concat('.in:');
   object.values.forEach((value) => {
@@ -65,7 +67,7 @@ function inOperation(object: SparkleObject): string {
 
 function notInOperation(object: SparkleObject): string {
   if (object.values.length < 1) {
-    throw Error('Operation requires atleast one value');
+    throw Error('Operation requires at least one value');
   }
   let resultString: string = object.bind.concat('.nin:');
   object.values.forEach((value) => {
@@ -93,5 +95,7 @@ function getFuncFromOperation(operation: Operation): (arg0: SparkleObject) => st
       return inOperation;
     case '!in':
       return notInOperation;
+    case 'like':
+      return likeOperation;
   }
 }
